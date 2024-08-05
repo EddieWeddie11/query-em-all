f25ae13d495d3b08a7cfed426eefd6f30885ee83
edward-soung-24 <170850092+EddieWeddie11@users.noreply.github.com>
2024-08-06 10:41:30 +1200

    Setup Done

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { routes } from './routes.tsx'
 
 const router = createBrowserRouter(routes)
+const queryClient = new QueryClient()
 
 const root = createRoot(document.getElementById('app') as HTMLElement)
-root.render(<RouterProvider router={router} />)
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
+)
