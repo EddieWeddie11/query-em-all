acedf835a8c27bca1b5dbb9dfde418dc1215f35d
edward-soung-24 <170850092+EddieWeddie11@users.noreply.github.com>
2024-08-06 15:00:27 +1200

    Step 4, point 3

--- client/apis/pokemon.ts
+++ client/apis/pokemon.ts
@@ -3,13 +3,13 @@ import type { Pokemon, PokemonGeneration } from '../../models/pokemon.ts'
 
 export async function fetchPokemonGeneration(generation: number) {
   const res = await request.get(
-    `https://pokeapi.co/api/v2/generation/${generation}`
+    `https://pokeapi.co/api/v2/generation/${generation}`,
   )
 
   return res.body as PokemonGeneration
 }
 
-export async function fetchPokemonByName(name: string | undefined, String: StringConstructor, name: string) {
+export async function fetchPokemonByName(name: string) {
   const res = await request.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
   return res.body as Pokemon
 }
