a53a45a1d3d2a9df5d38c85a0ebe199606381ad6
olivia-burgess <171209277+olivia-burgess@users.noreply.github.com>
2024-08-06 12:55:16 +1200

    Working through Step 2

--- client/components/PokemonList.tsx
+++ client/components/PokemonList.tsx
@@ -2,11 +2,28 @@ import { PokemonGeneration } from '../../models/pokemon.ts'
 import { useQuery } from '@tanstack/react-query-devtools'
 import { fetchPokemonGeneration } from '../apis/pokemon.ts'
 
-function GetPokemonName() {
-const { data: pokemonNames, error, isLoading } = useQuery({ queryKey: ['pokemonNames'], queryFn: fetchPokemonGeneration})
-}
+// function GetPokemonName() {
+// const { data: pokemonNames, error, isLoading } = useQuery({ queryKey: ['pokemonNames'], queryFn: fetchPokemonGeneration})
+// }
+
+const generationNum = 1 //this is so different generations will be saved on client side without require refetch
 
 export default function PokemonList() {
+  const { isPending, isError, error, data } = useQuery({
+    queryKey: ['pokemonGeneration', generationNum],
+    queryFn: () => fetchPokemonGeneration(generationNum),
+  })
+
+  if (isPending) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    console.log(error.message)
+    return <p>Error</p>
+  }
+
+  const generation = data as PokemonGeneration
   return (
     <>
       <h2>Pokémon in {generation.main_region.name}:</h2>
@@ -19,11 +36,11 @@ export default function PokemonList() {
   )
 }
 
-const generation = {
-  id: 1,
-  main_region: { name: 'Kanto', url: 'https://pokeapi.co/api/v2/region/1/' },
-  name: 'generation-i',
-  pokemon_species: [
-    { url: 'https://pokeapi.co/api/v2/pokemon/bulbasaur', name: 'Bulbasaur' },
-  ],
-} as PokemonGeneration
+// const generation = {
+//   id: 1,
+//   main_region: { name: 'Kanto', url: 'https://pokeapi.co/api/v2/region/1/' },
+//   name: 'generation-i',
+//   pokemon_species: [
+//     { url: 'https://pokeapi.co/api/v2/pokemon/bulbasaur', name: 'Bulbasaur' },
+//   ],
+// } as PokemonGeneration
